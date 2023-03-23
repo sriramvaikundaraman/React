674ed6fe8d6ecbbba1e0d25c9c46157c78a25419
Sriram <sriramvaikundaraman@gmail.com>
2023-03-23 20:09:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,20 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div>
+      <header >
         <a
-          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <h1>Sriram</h1>
       </header>
     </div>
   );
